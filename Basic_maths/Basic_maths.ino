17dd8cb5547c321876eae9ca9da7ce20bdc609ac
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2022-10-17 11:39:21 +0530

    add maths * - +

--- Basic_maths/Basic_maths.ino
+++ Basic_maths/Basic_maths.ino
@@ -6,6 +6,15 @@ void setup() {
   int v3=v1+v2;
   Serial.println(v3);
 
+  int v4=v1-v2;
+  Serial.println(v3);
+  
+  int v5=v1*v2;
+  Serial.println(v3);
+  
+  int v6=v1/v2;
+  Serial.println(v3);
+
 }
 
 void loop() {
